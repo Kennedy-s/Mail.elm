d4771f0734ded2a5da848171cf21d7236759627d
Kennedy <kennedys@miranetworks.net>
2017-05-25 13:45:52 +0200

    message

--- mail.elm
+++ mail.elm
@@ -305,15 +305,11 @@ view : Model -> Html Msg
 view model =
   case model.message == "Ok" of
     True ->
-      messagePage model
+      sendView model
     False ->
       loginPage model
   
-     
-
-
-
-
+  
 
 -- Subscriptions
 
