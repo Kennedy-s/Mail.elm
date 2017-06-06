1b1ad0f1ecc3a8a34d9d2493d3e2ea9609f3e538
Kennedy <kennedys@miranetworks.net>
2017-06-06 13:56:01 +0200

    message

--- mail.elm
+++ mail.elm
@@ -269,7 +269,7 @@ inboxPage model =
 addInboxMessage inboxMessage = 
   li [] [ text inboxMessage.messageBody
         , button [onClick ( Delete  "delete"), value "Delete" ] [ text "delete"]
-        , button [onClick ( Reply   "reply"), value "Reply" ] [ text "reply"]
+        , button [onClick ( Reply   "reply"), value "Reply"  ] [ text "reply"]
         ]
 
            
