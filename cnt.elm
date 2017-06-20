f03e72e900a1e856b9651545df210bfff4e5977c
Kennedy <kennedys@miranetworks.net>
2017-06-20 11:03:02 +0200

    message

---
+++ cnt.elm
@@ -0,0 +1,57 @@
+module Cnt exposing (..)
+
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html exposing (..)
+import Http exposing (..)
+
+
+main =
+ Html.program
+  { init = init 
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
+
+
+--Model 
+  
+type alias Model =
+   { 
+
+
+   }
+
+model : Model
+model =
+
+
+
+init : ( Model, Cmd Msg)
+init =
+  ( model, Cmd.none)
+
+
+  --Update 
+
+
+type Msg 
+    =
+
+update : Msg -> Model -> (Model, Cmd msg)
+update msg model  =
+  case msg of
+
+
+--View 
+
+view : Model -> Html Msg
+view model =
+
+
+--Subscriptions 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+ Sub.none
