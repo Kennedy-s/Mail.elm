4e5a41927cb5bdf762c3817c105005f052307c0d
Kennedy <kennedys@miranetworks.net>
2017-07-05 14:41:59 +0200

    message

--- index.elm
+++ index.elm
@@ -73,7 +73,7 @@ type Msg
    | Draft String
    | MailPage String
    | Message String
-   | Submit String
+  
 
 
 
@@ -166,7 +166,6 @@ view model =
     [ input [ type_ "text" ] [ text "mailPage" ]
     , input [ type_ "text" ] [ text "sentPage" ]
     , input [ type_ "text" ] [ text "filterpage" ]
-    , button [ onClick Submit ] [ text "Submit" ]
     ]
 
 
