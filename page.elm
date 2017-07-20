686ed6b32b4afc39850797ffddc22334859f6189
Kennedy <kennedys@miranetworks.net>
2017-07-20 14:37:46 +0200

    message

---
+++ page.elm
@@ -0,0 +1,57 @@
+module Page exposing (..)
+
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html exposing (..)
+import Http exposing (..)
+
+
+main =
+  Html.program
+     { init = init
+     , view = view 
+     , update = update
+     , subscriptions = subscriptions
+     }
+
+
+--Model
+
+type alias Model =
+    
+
+
+model : Model
+model =
+  
+
+init : ( model, Cmd Msg )
+init =
+    ( model, Cmd.none )
+
+
+
+--Update 
+
+type Msg
+   
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+
+
+
+--View 
+
+view : Model -> Html Msg
+view model =
+   div []
+
+
+--Subscriptions 
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
