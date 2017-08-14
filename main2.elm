244da3195ed24260769c9ef4305b1223619d499d
Kennedy <kennedys@miranetworks.net>
2017-08-14 14:18:56 +0200

    message

--- main2.elm
+++ main2.elm
@@ -3,14 +3,7 @@ module Main2 exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Navigation exposing (..)
-import Bootstrap.Navbar as Navbar
-import Bootstrap.Grid as Grid 
-import Bootstrap.Grid.Col as Col
-import Bootstrap.Card as Card
-import Bootstrap.Button as Button
-import Bootstrap.ListGroup as Listgroup
-import Bootstrap.Modal as Modal
+import Http  exposing (..)
 
 
 main : Program Never Model Msg
