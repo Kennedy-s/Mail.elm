43bf186cceb0e0344ddb2241c77c82b76f504218
Kennedy <kennedys@miranetworks.net>
2017-08-14 12:05:22 +0200

    message

---
+++ main2.elm
@@ -0,0 +1,128 @@
+module Main2 exposing (..)
+
+import Html exposing (..)
+import Html.Attrobutes exposing (..)
+import Html.Events exposing (..)
+import Navigation exposing (..)
+import UrlParser exposing (..)
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Grid as Grid 
+import Bootstrap.Grid.Col as col
+import Bootstrap.Card as Card
+import Bootstrap.Button as button
+import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Modal as Modal
+
+
+main : Program Never Model Msg
+main = 
+    Navigation.Program UrlChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+--Model
+
+type alias Model
+    { page : page
+    , navState : Navbar.State
+    , modal : Modal.State
+    }
+
+modal : Model -> Html Msg
+modal model =
+    Modal.config ModalMsg
+        |> Modal.small
+        |> Modal.h4 [] [ text "Getting started ?" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 1" ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 2" ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.modalState
+
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        ( navState, navCmd ) =
+            Navbar.initialState NavMsg
+
+        ( model, urlCmd ) =
+            urlUpdate location { navState = navState, page = Home, modalState = Modal.hiddenState }
+    in
+        ( model, Cmd.batch [ urlCmd, navCmd ] )
+
+--Update
+
+type Msg
+    = UrlChange Location
+    | NavMsg Navbar.State
+    | ModalMsg Modal.State
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlChange location ->
+            urlUpdate location model
+
+        NavMsg state ->
+            ( { model | navState = state }
+            , Cmd.none
+            )
+
+        ModalMsg state ->
+            ( { model | modalState = state }
+            , Cmd.none
+            )
+
+
+urlUpdate : Navigation.Location -> Model -> ( Model, Cmd Msg )
+urlUpdate location model =
+    case decode location of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just route ->
+            ( { model | page = route }, Cmd.none )
+
+
+decode : Location -> Maybe Page
+decode location =
+    UrlParser.parseHash routeParser location
+
+
+routeParser : UrlParser.Parser (Page -> a) a
+routeParser =
+    UrlParser.oneOf
+        [ UrlParser.map Home UrlParser.top
+        , UrlParser.map GettingStarted (UrlParser.s "getting-started")
+        , UrlParser.map Modules (UrlParser.s "modules")
+
+
+--View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ menu model
+        , mainContent model
+        , modal model
+        ]
+
+
+--Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Navbar.subscriptions model.navState NavMsg
